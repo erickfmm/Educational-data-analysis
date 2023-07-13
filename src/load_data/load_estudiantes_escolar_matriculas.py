0de5dfa2ea79f64ccac4aeca219ffc927c7e3356
Erick Merino <erick.merino@mineduc.cl>
2023-07-13 17:53:13 -0400

    load data in chunks to fix memory issues

--- src/load_data/load_estudiantes_escolar_matriculas.py
+++ src/load_data/load_estudiantes_escolar_matriculas.py
@@ -160,81 +160,82 @@ def insert_df(conn, bd: str):
         full_path = join(BASE_FOLDER, file_path)
         #print(full_path)
         # Load the file into a DataFrame
-        if file_path.endswith(".csv"):
-            df = pd.read_csv(full_path, sep=";", on_bad_lines="warn", low_memory=False, encoding="iso 8859-1")
-        if file_path.endswith(".xls") or file_path.endswith(".xlsx"):
-            df = pd.read_excel(full_path, 0)
-        print("to reindex")
-        mycols = []
-        for name in df.columns:
-            mycols.append(name.upper())
-        df.columns = mycols
-        df = df.reindex(columns=COMMON_COLUMNS)
-        df = df[COMMON_COLUMNS]
+        chunks = pd.read_csv(full_path, sep=";", on_bad_lines="warn", low_memory=False, chunksize=10**6, encoding="iso 8859-1")
+        i_chunk = 0
+        for df in chunks:
+            print("chunk ", i_chunk)
+            i_chunk += 1
+            print("to reindex")
+            mycols = []
+            for name in df.columns:
+                mycols.append(name.upper())
+            df.columns = mycols
+            df = df.reindex(columns=COMMON_COLUMNS)
+            df = df[COMMON_COLUMNS]
 
-        for col in int_columns:
-            df[col] = df[col].apply(to_int).astype('Int64')
+            for col in int_columns:
+                df[col] = df[col].apply(to_int).astype('Int64')
 
-        if bd == "spark":
-            print("to create spark")
-            sdf = conn.createDataFrame(data=df, schema=schema)
-            sdf.printSchema()
-            print("to write")
-            #sdf.write.mode('append').saveAsTable('estudiantes_parvularia_matricula')
-            sdf.write.mode('append').format('hive').saveAsTable('estudiantes_escolar_matricula')
-        if bd == "postgres":
-            print(len(df))
-            i_rows = 0
-            for index, row in df.iterrows():
-                mirow = clean_row_forpgsql(row)
-                miinsert = f'INSERT INTO estudiantes_escolar_matricula(AGNO,RBD,DGV_RBD,NOM_RBD,COD_REG_RBD,NOM_REG_RBD_A,COD_PRO_RBD,COD_COM_RBD,NOM_COM_RBD,COD_DEPROV_RBD,NOM_DEPROV_RBD,COD_DEPE,COD_DEPE2,RURAL_RBD,ESTADO_ESTAB,COD_ENSE,COD_ENSE2,COD_ENSE3,COD_GRADO,COD_GRADO2,LET_CUR,COD_JOR,COD_TIP_CUR,COD_DES_CUR,MRUN,GEN_ALU,FEC_NAC_ALU,EDAD_ALU,COD_REG_ALU,COD_COM_ALU,NOM_COM_ALU,COD_SEC,COD_ESPE,COD_RAMA,COD_MEN,ENS) VALUES(\
-    {mirow["AGNO"]},\
-{mirow["RBD"]},\
-{mirow["DGV_RBD"]},\
-{mirow["NOM_RBD"]},\
-{mirow["COD_REG_RBD"]},\
-{mirow["NOM_REG_RBD_A"]},\
-{mirow["COD_PRO_RBD"]},\
-{mirow["COD_COM_RBD"]},\
-{mirow["NOM_COM_RBD"]},\
-{mirow["COD_DEPROV_RBD"]},\
-{mirow["NOM_DEPROV_RBD"]},\
-{mirow["COD_DEPE"]},\
-{mirow["COD_DEPE2"]},\
-{mirow["RURAL_RBD"]},\
-{mirow["ESTADO_ESTAB"]},\
-{mirow["COD_ENSE"]},\
-{mirow["COD_ENSE2"]},\
-{mirow["COD_ENSE3"]},\
-{mirow["COD_GRADO"]},\
-{mirow["COD_GRADO2"]},\
-{mirow["LET_CUR"]},\
-{mirow["COD_JOR"]},\
-{mirow["COD_TIP_CUR"]},\
-{mirow["COD_DES_CUR"]},\
-{mirow["MRUN"]},\
-{mirow["GEN_ALU"]},\
-{mirow["FEC_NAC_ALU"]},\
-{mirow["EDAD_ALU"]},\
-{mirow["COD_REG_ALU"]},\
-{mirow["COD_COM_ALU"]},\
-{mirow["NOM_COM_ALU"]},\
-{mirow["COD_SEC"]},\
-{mirow["COD_ESPE"]},\
-{mirow["COD_RAMA"]},\
-{mirow["COD_MEN"]},\
-{mirow["ENS"]}\
-                );'
-                try:
-                    _ = cur.execute(miinsert)
-                except Exception as e:
-                    print(mirow)
-                    print()
-                    print(miinsert)
-                    print()
-                    print(e)
-                    return
-                i_rows += 1
-                if i_rows % 1000 == 0:
-                    print(i_rows)
-                    conn.commit()
+            if bd == "spark":
+                print("to create spark")
+                sdf = conn.createDataFrame(data=df, schema=schema)
+                sdf.printSchema()
+                print("to write")
+                #sdf.write.mode('append').saveAsTable('estudiantes_parvularia_matricula')
+                sdf.write.mode('append').format('hive').saveAsTable('estudiantes_escolar_matricula')
+            if bd == "postgres":
+                print(len(df))
+                i_rows = 0
+                for index, row in df.iterrows():
+                    mirow = clean_row_forpgsql(row)
+                    miinsert = f'INSERT INTO estudiantes_escolar_matricula(AGNO,RBD,DGV_RBD,NOM_RBD,COD_REG_RBD,NOM_REG_RBD_A,COD_PRO_RBD,COD_COM_RBD,NOM_COM_RBD,COD_DEPROV_RBD,NOM_DEPROV_RBD,COD_DEPE,COD_DEPE2,RURAL_RBD,ESTADO_ESTAB,COD_ENSE,COD_ENSE2,COD_ENSE3,COD_GRADO,COD_GRADO2,LET_CUR,COD_JOR,COD_TIP_CUR,COD_DES_CUR,MRUN,GEN_ALU,FEC_NAC_ALU,EDAD_ALU,COD_REG_ALU,COD_COM_ALU,NOM_COM_ALU,COD_SEC,COD_ESPE,COD_RAMA,COD_MEN,ENS) VALUES(\
+        {mirow["AGNO"]},\
+    {mirow["RBD"]},\
+    {mirow["DGV_RBD"]},\
+    {mirow["NOM_RBD"]},\
+    {mirow["COD_REG_RBD"]},\
+    {mirow["NOM_REG_RBD_A"]},\
+    {mirow["COD_PRO_RBD"]},\
+    {mirow["COD_COM_RBD"]},\
+    {mirow["NOM_COM_RBD"]},\
+    {mirow["COD_DEPROV_RBD"]},\
+    {mirow["NOM_DEPROV_RBD"]},\
+    {mirow["COD_DEPE"]},\
+    {mirow["COD_DEPE2"]},\
+    {mirow["RURAL_RBD"]},\
+    {mirow["ESTADO_ESTAB"]},\
+    {mirow["COD_ENSE"]},\
+    {mirow["COD_ENSE2"]},\
+    {mirow["COD_ENSE3"]},\
+    {mirow["COD_GRADO"]},\
+    {mirow["COD_GRADO2"]},\
+    {mirow["LET_CUR"]},\
+    {mirow["COD_JOR"]},\
+    {mirow["COD_TIP_CUR"]},\
+    {mirow["COD_DES_CUR"]},\
+    {mirow["MRUN"]},\
+    {mirow["GEN_ALU"]},\
+    {mirow["FEC_NAC_ALU"]},\
+    {mirow["EDAD_ALU"]},\
+    {mirow["COD_REG_ALU"]},\
+    {mirow["COD_COM_ALU"]},\
+    {mirow["NOM_COM_ALU"]},\
+    {mirow["COD_SEC"]},\
+    {mirow["COD_ESPE"]},\
+    {mirow["COD_RAMA"]},\
+    {mirow["COD_MEN"]},\
+    {mirow["ENS"]}\
+                    );'
+                    try:
+                        _ = cur.execute(miinsert)
+                    except Exception as e:
+                        print(mirow)
+                        print()
+                        print(miinsert)
+                        print()
+                        print(e)
+                        return
+                    i_rows += 1
+                    if i_rows % 1000 == 0:
+                        print(i_rows)
+                        conn.commit()
