fd8d35e3111b0b6601ec48e6e8d2cc32550fbe00
Erick Merino <erick.merino@mineduc.cl>
2023-08-03 12:42:51 -0400

    get text from pdfs

---
+++ src/process_data/get_text_from_pdfs.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import fitz
+
+def read_text(folder):
+    for filename in os.listdir(folder):
+        filename : str = filename
+        if not filename.endswith(".pdf"):
+            continue
+        print(filename)
+        doc = fitz.open(os.path.join(folder, filename))
+        text = ""
+        for page in doc:
+            text+=page.get_text()
+        with open(os.path.join(folder, f"text_{filename}.txt"), "w", encoding="utf-8", errors="warn") as fh:
+            fh.write(text)
+        
+if __name__ == "__main__":
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+    read_text("files_pei")
